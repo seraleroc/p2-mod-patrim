8b9ba4ec9f4c2b210b8a5aa541fca0550681a19a
Rosana <rcathayde2012@gmail.com>
2022-11-23 17:47:50 -0300

    Componente Modal

---
+++ src/components/ModalCreateMat.js
@@ -0,0 +1,153 @@
+import { Button, Modal, Row, Col, Form } from "react-bootstrap";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import axios from "axios";
+
+function ModalCreateMat({ reload, setReload }) {
+    const [show, setShow] = useState(false);
+    const [form, setForm] = useState({
+        rp: "",
+        item: "",
+        itemdetalhe: "",
+        sitfisica: "",
+        valcompra: "0",
+        localiz: "",
+        observ: "",
+    });
+
+const handleClose = () => setShow(false);
+const handleShow = () => setShow(true);
+
+function handleChange(e) {
+    setForm({ ...form, [e.target.name]: e.target.value });
+}
+
+async function handleSubmit(e) {
+   e.preventDefault();
+   try {
+    await axios.post("https://ironrest.cyclic.app/prj02modequipsiads", form);
+    handleClose();
+    setForm({
+        rp: "",
+        item: "",
+        itemdetalhe: "",
+        sitfisica: "",
+        valcompra: "0",
+        localiz: "",
+        observ: "",
+    });
+    toast.success("Material cadastrado com sucesso! :D");
+    setReload(!reload);
+   } catch (error) {
+     console.log (error);
+     toast.error("Error. Tente novamente");
+   }
+}
+return (
+    <div>
+        <Button variant="success" onClick={handleShow}>
+            + Cadastrar novo material
+        </Button>
+
+    <Modal show={show} onHide={handleClose} size="xl">
+        <Modal.Header closeButton>
+            <Modal.Title>Form Cadastro de Material</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            <Form>
+                <Row>
+                    <Col>
+                        <Form.Group className="mb-3">
+                        <Form.Label>Nome do Material</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="Digite o nome do material"
+                            name="item"
+                            value={form.item}
+                            onChange={handleChange}
+                            autoFocus
+                        />
+                        </Form.Group>
+                    </Col>
+                    <Col>
+                        <Form.Group className="mb-3">
+                        <Form.Label>Descrição do Material</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="Digite a descrição do material"
+                            name="itemdetalhe"
+                            value={form.itemdetalhe}
+                            onChange={handleChange}
+                        />
+                        </Form.Group>
+                    </Col>   
+                </Row>
+                <Row>
+                <Col>
+                        <Form.Group className="mb-3">
+                        <Form.Label>Situação do Material</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="Qual a Situação do Material"
+                            name="sitfisica"
+                            value={form.sitfisica}
+                            onChange={handleChange}
+                        />
+                        </Form.Group>
+                </Col>  
+                    <Col>
+                        <Form.Group className="mb-3">
+                        <Form.Label>Valor Inicial de Compra</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="Valor de Compra"
+                            name="valcompra"
+                            value={form.valcompra}
+                            onChange={handleChange}
+                        />
+                        </Form.Group>
+                </Col>  
+                </Row>
+                <Row>
+                <Col>
+                        <Form.Group className="mb-3">
+                        <Form.Label>Localização</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="Qual a Localização do Material"
+                            name="localiz"
+                            value={form.localiz}
+                            onChange={handleChange}
+                        />
+                        </Form.Group>
+                </Col>  
+                    <Col>
+                        <Form.Group className="mb-3">
+                        <Form.Label>Observação</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="Observação"
+                            name="observ"
+                            value={form.observ}
+                            onChange={handleChange}
+                        />
+                        </Form.Group>
+                </Col>  
+                </Row>
+            </Form>
+        </Modal.Body>
+        <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+                Cancelar            
+            </Button>
+            <Button variant="primary" onClick={handleSubmit}>
+                Cadastrar Material
+            </Button>
+        </Modal.Footer>
+    </Modal>
+    </div>
+)
+
+};
+
+export default ModalCreateMat;
