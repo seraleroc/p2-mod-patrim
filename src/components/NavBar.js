c71dcfffb492de7c7e7bc05e2c72d593f2703c3a
Sergio Rocha <sergio.rocha.pa@gmail.com>
2022-11-23 12:11:36 -0300

    Novo Layout Homepage 23.11.22

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,18 +4,18 @@ import {Link} from "react-router-dom";
 function NavBar() {
 
     return (
-        <Navbar bg="dark" variant="dark">
+        <Navbar bg="green" variant="green">
             <Container>
                 <Link to="/" style={{ textDecoration: "none"}}>
                     <Navbar.Brand as="div">
                         <img
                             alt="logo SIADS"
                             src="https://siads.fazenda.gov.br/siadsweb/resources/img/logo-siads.png"
-                            width="30"
-                            height="30"
-                            className="d-inline-block align-top"
+                            width="100"
+                            height="100"
+                            className="d-inline-block align-center"
                         />{' '}
-                        Módulo Estoque - Nova Entrada de Material
+                        <strong>Módulo Estoque - Nova Entrada de Material</strong>
                     </Navbar.Brand>
                 </Link>         
             </Container>
