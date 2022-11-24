539f0a2cd5ff395047d98c96e22d5a55928105dc
seraleroc <sergio.rocha.pa@gmail.com>
2022-11-23 23:03:18 -0300

    Homepage e ModalCreateMat Funcionando 23h03

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,15 +1,62 @@
-
+import { Table, Container, Button } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import ModalCreateMat from "../components/ModalCreateMat";
 
 function HomePage() {
-    return (
-        <div>
-            <h1> Homepage</h1>
-            <p> Pagina Inicial do Módulo de Cadastro / Consulta de Equipamentos</p>
+  const [items, setItems] = useState([]);
 
-        </div>
+  useEffect(() => {
+    async function fetchItems() {
+      const response = await axios.get(
+        "https://ironrest.cyclic.app/prj02modequipsiads2"
+      );
+      //console.log(response);
+      setItems(response.data);
+    }
 
-    );
-    
+    fetchItems();
+  }, []);
+
+  //console.log(items)
+
+  return (
+    <div>
+      <h1> Módulo de Cadastro / Consulta de Equipamentos</h1>
+      <tr> </tr>
+
+      <Container>
+        <Table striped bordered hover size="sm">
+          <thead>
+            <tr>
+              <th>Número do Patrimônio</th>
+              <th>Equipamento</th>
+              <th>Estado de Conservação</th>
+              <th>Localização</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {items.map((item) => {
+              return (
+                <tr key={item._id}>
+                  <td>{item.rp}</td>
+                  <td>{item.item}</td>
+                  <td>{item.sitFisica}</td>
+                  <td>{item.localiz}</td>
+                  <td>
+                    <Button variant="outline-primary">Detalhes</Button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+
+        <ModalCreateMat />
+      </Container>
+    </div>
+  );
 }
 
 export default HomePage;
